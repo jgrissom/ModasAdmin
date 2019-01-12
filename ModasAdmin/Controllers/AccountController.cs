2a2508cd4bffb6a8e50c177dd899b4d10745299c
jgrissom <jgrissom91@gmail.com>
2019-01-12 11:43:12 -0600

    Modify Account Controller
    Implement the Login method (post requests)

--- ModasAdmin/Controllers/AccountController.cs
+++ ModasAdmin/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ModasAdmin.Models;
 
@@ -8,6 +9,15 @@ namespace ModasAdmin.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userMgr, SignInManager<AppUser> signInMgr)
+        {
+            userManager = userMgr;
+            signInManager = signInMgr;
+        }
+
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)
         {
@@ -20,6 +30,20 @@ namespace ModasAdmin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel details, string returnUrl)
         {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(user, details.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user or password");
+            }
             return View(details);
         }
     }
