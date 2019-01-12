7565cc4027d682340e5db0091cdccdf6b5c62624
jgrissom <jgrissom91@gmail.com>
2019-01-12 08:18:21 -0600

    Home/Index
    1) Add the Home Controller
    2) Add the Index Controller Method
    3) Add the Home/Index View

---
+++ ModasAdmin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModasAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
