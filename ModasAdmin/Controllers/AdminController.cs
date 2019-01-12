6cbf773e9574c6829ccf749efa21ce4c5a4cd419
jgrissom <jgrissom91@gmail.com>
2019-01-11 23:01:34 -0600

    Create the controller method that responds to post requests for Admin/Create

--- ModasAdmin/Controllers/AdminController.cs
+++ ModasAdmin/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ModasAdmin.Models;
 
@@ -16,5 +17,32 @@ namespace ModasAdmin.Controllers
         public ViewResult Index() => View(userManager.Users);
 
         public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
     }
 }
