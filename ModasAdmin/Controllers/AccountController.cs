4088e3186332fa16ecba2164b539c91ef5ce85a4
jgrissom <jgrissom91@gmail.com>
2019-01-12 08:48:23 -0600

    Create the Account Controller
    1) Create the Login Controller method (get requests)
    2) Create the Login Controller method (post requests

---
+++ ModasAdmin/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ModasAdmin.Models;
+
+namespace ModasAdmin.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel details, string returnUrl)
+        {
+            return View(details);
+        }
+    }
+}
