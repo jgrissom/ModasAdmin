6b9f4b2421a5e7603225d05061f3ef0780013679
jgrissom <jgrissom91@gmail.com>
2019-01-11 22:37:55 -0600

    Admin Controller class
    1) Create the Admin Controller class
    2) Create a UserManager object to query the data collection
    3) Create the Index Action Method that will send the enumerated Users to the view

---
+++ ModasAdmin/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ModasAdmin.Models;
+
+namespace ModasAdmin.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+
+        public AdminController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        public ViewResult Index() => View(userManager.Users);
+    }
+}
